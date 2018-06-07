ac2d9224e06998542b5d2b4dbb9a333eb0aa05eb
yannisB <yannis.bourree@MINEMIC-6OJ8J05>
2018-06-07 12:59:19 +0200

    fond temporaire

--- index.php
+++ index.php
@@ -5,38 +5,37 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html class="">
     <head>
         <meta charset="UTF-8">
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
         <title>Acceuil</title>
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-        <img alt="Image du jeu/ Tuto/Video">
-        <div class="Contenue">
-            <div>
-                Connexion/Inscription
-                <form>
-                    <label for="id_Identifiant">Identifiant:</label>
-                    <input id="id_Identifiant" maxlength="100" type="text">
-                    
-                    <label for="id_MDP">Mot de passe:</label>
-                    <input id="id_MDP" maxlength="100" type="text">
-                    
-                    <input type="submit" value="CONNECTION" id="envoyerCV">
-                </form>
-            </div>
-            <div>
-                Explications
-            </div>
+    <body class="bg-img text-center">
+        <div class="zone_co">
+            <form class="form-signin">
+                <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+                
+                <label for="inputIdentifiant" class="sr-only">Mail ou Identifiant</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Mail ou Identifiant" required autofocus>
+                
+                <label for="inputPassword" class="sr-only">Mot de passe</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+                
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Rester connecté.
+                    </label>
+                </div>
+                <p>Pas encore de compte?<a href="#">S'inscrire.</a></p>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
         </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
     </body>
 </html>
