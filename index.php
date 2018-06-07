37aea76932c7fcc11304ff87adf7ea72598bb227
yannisB <yannis.bourree@MINEMIC-6OJ8J05>
2018-06-07 13:13:52 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- index.php
+++ index.php
@@ -19,7 +19,10 @@ and open the template in the editor.
                 <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
                 
                 <label for="inputIdentifiant" class="sr-only">Mail ou Identifiant</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Mail ou Identifiant" required autofocus>
+                <input type="id" id="inputIdentifiant" class="form-control" placeholder="Mail ou Identifiant" required autofocus>
+                
+                <label for="inputMail" class="sr-only">Adresse Mail</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Adresse Mail" required autofocus>
                 
                 <label for="inputPassword" class="sr-only">Mot de passe</label>
                 <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
@@ -30,7 +33,7 @@ and open the template in the editor.
                     </label>
                 </div>
                 <p>Pas encore de compte?<a href="#">S'inscrire.</a></p>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
             </form>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
