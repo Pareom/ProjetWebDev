d08553047f4146da1c912cb72339ce9112a99697
Camille <Camille@PC-Camille>
2018-06-07 22:12:59 +0200

    Merge origin/master
    Conflicts:
    	index.php
    	nbproject/project.properties

--- compte.php
+++ compte.php
@@ -1,213 +1,174 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
         <title>Page compte</title>
     </head>
-    <body>
+    <body id="body_compte">
+        <div class="container-fluid">
         <?php
         // put your code here
         ?>
-        <div id="head">
-            <img id="avatar" alt="Avatar du joueur">
-            <h1 id="pseudo"></h1>
-            <h3 id="classement_joueur">Classement : </h3>
+        <div id="head" class="row">
+            <img src="img/avatar_test.png" class="rounded-circle col-sm-3 img-fluid" id="avatar" alt="Avatar">
+            <h1 class="col-sm-7 align-item-center" id="pseudo">Pseudo</h1>
+            <h3 class="col-sm-2" id="classement_joueur">Classement : </h3>
         </div>
-        <button type="button" id="jouer">Jouer</button>
-        <div id="scores">
-            <table id="classement_general">
-                <thead>
-                <th>Classement général</th>
-                <button type="button" id="mon_classement">Mon classement</button>
-                    <tr>
-                        <th>#</th>
-                        <th>Joueur</th>
-                        <th>Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>1</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>4</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>5</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>6</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>7</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>7</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>8</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>9</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>10</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                </tbody>
-            </table>
-            <table id="classement_perso">
-                <thead>
-                <th>Classement général</th>
-                    <tr>
-                        <th>#</th>
-                        <th>Joueur</th>
-                        <th>Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>1</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>4</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>5</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>6</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>7</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>7</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>8</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>9</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>10</th>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                </tbody>
-            </table>
-            <table id="classement_perso">
-                <thead>
-                <th>Scores personnels</th>
-                <select>
-                    <option>trier par scores</option>
-                    <option>trier par dates</option>
-                </select>
-                    <tr>
-                        <th>Scores</th>
-                        <th>Dates</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                    <tr>
-                        <th>-</th>
-                        <th>-</th>
-                    </tr>
-                <ul class="pagination">
-                    <li class="page-item"><a class="page-link" href="#"><</a></li>
-                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">></a></li>
-                </ul>
-                </tbody>
-            </table>
+        <button type="button" id="jouer" class="btn btn-warning btn-primary btn-lg">Jouer</button>
+        <div id="scores" class="row">
+            <div class="col">
+                <table id="classement_general" class="table table-dark table-striped">
+                    <thead>
+                        <tr>    
+                            <th>Classement général</th>
+                            <th></th>
+                            <th><button type="button" id="mon_classement" class="btn btn-secondary btn-sm">Mon classement</button></th>
+                        </tr>
+                        <tr>
+                                <th>#</th>
+                                <th class= sm-2>Joueur</th>
+                                <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>3</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>4</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>5</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>6</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>7</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>8</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>9</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>10</th>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <th></th>
+                        <th class="col-md3">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#"><</a></li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">></a></li>
+                            </ul>
+                        </th>
+                        <th></th>
+                    </tfoot>
+                </table>
+            </div>
+            <div class="col">
+                <table id="classement_perso" class="table table-dark table-striped">
+                    <thead>
+                    <th>Scores personnels</th>
+                    <th>
+                        <select id="trier" class="btn btn-secondary btn-sm">
+                            <option>trier par scores</option>
+                            <option>trier par dates</option>
+                        </select>
+                    </th>
+                        <tr>
+                            <th>Scores</th>
+                            <th>Dates</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>
+                        <tr>
+                            <th>-</th>
+                            <th>-</th>
+                        </tr>  
+                    </tbody>
+                    <tfoot>
+                    <th></th>
+                    <th class="col-md3">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#"><</a></li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">></a></li>
+                        </ul>
+                    </th>
+                    <th></th>
+                    </tfoot>
+                </table>
+            </div>
         </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
     </body>
