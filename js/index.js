9f371247a1ba34e4ee22bc856d62982b55c11aba
yannisB <yannis.bourree@MINEMIC-6OJ8J05>
2018-06-10 14:19:57 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function Switch_Connection(){
+    console.log("passage en mode connection");
+    $("#titre").html("Connection");
+    $("#inputEmail").attr("style", "display:none;");
+    $("#inputIdentifiant").attr("placeholder", "EMail ou Identifiant");
+    $(".checkbox").attr("style", "display:block;");
+    $("#Switch").html("Pas encore de compte?<a href='#' onclick='Switch_Inscription()'>S'inscrire.</a>");
+    $(".btn").html("Connection");
+}
 
-
+function Switch_Inscription(){
+    console.log("Passage en mode inscription");
+    $("#titre").html("Inscription");
+    $("#inputEmail").attr("style", "display:block;");
+    $("#inputIdentifiant").attr("placeholder", "Identifiant");
+    $(".checkbox").attr("style", "display:none;");
+    $("#Switch").html("Déjà inscrit?<a href='#' onclick='Switch_Connection()'>Se connecter</a>");
+    $(".btn").html("Inscription");
+}
