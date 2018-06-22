a264fce9ed471e75e8efe0afe2d14dbfd42911f0
Camille <Camille@PC-Camille>
2018-06-22 14:17:17 +0200

    rien

--- js/compte.js
+++ js/compte.js
@@ -1,23 +1,37 @@
-function getXMLHttpRequest() {
-	var xhr = null;
+function request(oSelect) {
+	var value = oSelect.options[oSelect.selectedIndex].value;
+	var xhr   = getXMLHttpRequest();
 	
-	if (window.XMLHttpRequest || window.ActiveXObject) {
-		if (window.ActiveXObject) {
-			try {
-				xhr = new ActiveXObject("Msxml2.XMLHTTP");
-			} catch(e) {
-				xhr = new ActiveXObject("Microsoft.XMLHTTP");
-			}
-		} else {
-			xhr = new XMLHttpRequest(); 
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) { //Verifier que le serveur a bien effectué l'action
+			readData(xhr.responseXML);
+			document.getElementById("loader").style.display = "none";
+		} else if (xhr.readyState < 4) {
+			document.getElementById("loader").style.display = "inline";
 		}
-	} else {
-		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
-		return null;
-	}
+	};
 	
-	return xhr;
+	xhr.open("POST", "XMLHttpRequest_getListData.php", true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("IdEditor=" + value);
 }
+
+function readData(oData) {
+	var nodes   = oData.getElementsByTagName("item");
+	var oSelect = document.getElementById("softwaresSelect");
+	var oOption, oInner;
+	
+	oSelect.innerHTML = "";
+	for (var i=0, c=nodes.length; i<c; i++) {
+		oOption = document.createElement("option");
+		oInner  = document.createTextNode(nodes[i].getAttribute("name"));
+		oOption.value = nodes[i].getAttribute("id");
+		
+		oOption.appendChild(oInner);
+		oSelect.appendChild(oOption);
+	}
+}
+
 
 function changeAgar(){
     var jeu = document.getElementById("choixJeu");
@@ -25,24 +39,45 @@ function changeAgar(){
     var head = document.getElementById("head");
     jeu.innerHTML="Jouer à Agar.io";
     image.style.backgroundImage = "url('img/agar.png')";
+    head.style.backgroundImage = "url('img/agarTete.png')";;
 }
 function changePaper(){
     var jeu = document.getElementById("choixJeu");
+    var image = document.getElementById("scores");
+    var head = document.getElementById("head");
     jeu.innerHTML="Jouer à Paper.io";
+    image.style.backgroundImage = "url('img/paperFond.png')";
+    head.style.backgroundImage = "url('img/agar.png')";;
 }
 function changeSlither(){
     var jeu = document.getElementById("choixJeu");
+    var image = document.getElementById("scores");
+    var head = document.getElementById("head");
     jeu.innerHTML="Jouer à Slither.io";
+    image.style.backgroundImage = "url('img/agar.png')";
+    head.style.backgroundImage = "url('img/agar.png')";;
 }
 function changeSplix(){
     var jeu = document.getElementById("choixJeu");
+    var image = document.getElementById("scores");
+    var head = document.getElementById("head");
     jeu.innerHTML="Jouer à Splix.io";
+    image.style.backgroundImage = "url('img/agar.png')";
+    head.style.backgroundImage = "url('img/agar.png')";;
 }
 function changeSuperhex(){
     var jeu = document.getElementById("choixJeu");
+    var image = document.getElementById("scores");
+    var head = document.getElementById("head");
     jeu.innerHTML="Jouer à SuperHex.io";
+    image.style.backgroundImage = "url('img/agar.png')";
+    head.style.backgroundImage = "url('img/agar.png')";;
 }
 function changeOver(){
     var jeu = document.getElementById("choixJeu");
+    var image = document.getElementById("scores");
+    var head = document.getElementById("head");
     jeu.innerHTML="Jouer à OverWatch.io";
+    image.style.backgroundImage = "url('img/agar.png')";
+    head.style.backgroundImage = "url('img/agar.png')";;
 }
