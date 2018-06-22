419b090714e78f99c0485ea31963ee802c25ffa6
yannisB <yannis.bourree@MINEMIC-6OJ8J05>
2018-06-22 10:56:44 +0200

    debut AJAX

--- js/compte.js
+++ js/compte.js
@@ -1,7 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var xhr = null;
+	
+if (window.XMLHttpRequest || window.ActiveXObject) {
+	if (window.ActiveXObject) {
+		try {
+			xhr = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch(e) {
+			xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	} else {
+		xhr = new XMLHttpRequest(); 
+	}
+} else {
+	alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+}
 
 
